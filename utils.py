9fd8796a4a0912313c214e3ade0a0b2170eede91
Stepan Kharchenko <step.harch.2214@mail.ru>
2026-05-25 00:34:50 +0300

    develop(utils): add utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 from vkbottle.bot import Message
+from vkbottle import Bot
+import random as r
+
 from database.create_table import *
+import keyboards as kb
 
 from dotenv import load_dotenv
 import os
@@ -18,7 +22,7 @@ def user_stat(uid:int)->tuple:
         cursor.execute(f"SELECT AVG(ball) FROM results WHERE user_id = {uid} AND ball != -1")
         try: ball = cursor.fetchone()[0]*100
         except TypeError: ball = 0
-        cursor.execute(f"SELECT crated_at FROM users WHERE id = {uid}")
+        cursor.execute(f"SELECT created_at FROM users WHERE id = {uid}")
         date = cursor.fetchone()[0].split("-")
         date_created_at = dt.date(int(date[0]),int(date[1]),int(date[2]))
         date_now = dt.date.today()
@@ -40,8 +44,8 @@ def exersize_stat(eid:int)->int:
         return int(ball)
     
 def validate_theme(d:dict, message:Message)->bool:
-    return (d["stud"] == "Математика" and message.text in ("Алгебра","Геометрия","Вероятность и Статистика"))\
-        or (d["stud"] == "Физика" and message.text in ("Механика","МКТ и Термоденамика","Электромагнетизм","Квантовая"))
+    return (d["stud"] == "Математика" and message.text.strip() in ("Алгебра","Геометрия",kb.ver))\
+        or (d["stud"] == "Физика" and message.text.strip() in ("Механика","МКТ и Термоденамика","Электромагнетизм","Квантовая"))
     
 def add_exersizes_in_test(theme:str, add:bool, d:defaultdict)->dict:
     d[theme] += (1 if add else (-1 if d[theme]>0 else 0))
@@ -55,4 +59,30 @@ def end_of_test(d:defaultdict):
     
 def became_dict():
     return dict(ut_dct)
+
+async def add_test(message:Message, bot:Bot, d:dict):
+    if message.text in ("Физика","Математика"):
+        ut_dct = {ab:1 for th,ab in (kb.math_themes_list if message.text == "Математика" else kb.physic_themes_list)}
+        d["dct"] = ut_dct
+    else: d["dct"] = became_dict()
+    print(d["dct"])
+    math = any(i == j for i in d["dct"] for j in ("AL","GE","VE"))
+    var,uclass = max_value("results","var",f"user_id = {d['uid']}"),any_select("users",d["uid"])[3]
+    stud_themes_list = kb.math_themes_list if math else kb.physic_themes_list   #dict_of_themes: {тема(полностью):кортеж id-шек}
+    dict_of_themes = {i[0]:list(j[0] for j in select("study",
+                                                     f'theme = "{i[0]}" AND class = {uclass}',
+                                                     ("id",))) for i in stud_themes_list}
+    dict_of_themes_names = {i[1]:i[0] for i in stud_themes_list}   #dict_of_themes_names: {тема(аббревиатура):тема(полностью)}
+    exemples = []
+    print(dict_of_themes,dict_of_themes_names,sep="\n")
+    try:
+        for i in d["dct"]:#темы (сокращённо)
+            for j in range(d["dct"][i]):#нужное кол-во тем
+                ind = r.randint(0,len(dict_of_themes[dict_of_themes_names[i]])-1)
+                exemples.append(dict_of_themes[dict_of_themes_names[i]][ind])
+                del dict_of_themes[dict_of_themes_names[i]][ind]
+        insert("results",("user_id","exersize_id","var","ball"),
+               [(d["uid"],i,var+1,-1) for i in exemples])
+        return "Вариант составлен"
+    except IndexError: return "Не хватает заданий в базе данных"
     
